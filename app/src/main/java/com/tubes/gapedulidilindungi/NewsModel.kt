864d18482c9b3627dfca3b59794e46174ce08fb7
haikallf <haikallzrd@gmail.com>
2022-02-23 00:09:06 +0700

    create data class for news

---
+++ app/src/main/java/com/tubes/gapedulidilindungi/NewsModel.kt
@@ -0,0 +1,21 @@
+package com.tubes.gapedulidilindungi
+
+import java.util.*
+
+//"title": "Situasi COVID-19 di Indonesia (Update per 12 Februari 2022)",
+//"link": [
+//"https://covid19.go.id/artikel/2022/02/12/situasi-covid-19-di-indonesia-update-12-februari-2022"
+//],
+//"guid": "https://covid19.go.id/artikel/2022/02/12/situasi-covid-19-di-indonesia-update-12-februari-2022",
+//"pubDate": "Sat, 12 Feb 2022 19:45:00 +0700",
+//"description": {
+//    "__cdata": " <p>Situasi COVID-19 di Indonesia (Update per 12 Februari 2022)</p> "
+//},
+//"enclosure": {
+//    "_url": "https://covid19.go.id/storage/app/uploads/public/620/7ab/d42/6207abd427c6f835861278.jpeg",
+//    "_length": "196485",
+//    "_type": "image/jpeg"
+//}
+
+data class NewsModel (val title: String, val link: List<String>,
+                      val guid: String, val pubDate: String)
--- app/src/main/java/com/tubes/gapedulidilindungi/retrofit/ApiEndpoint.kt
+++ app/src/main/java/com/tubes/gapedulidilindungi/retrofit/ApiEndpoint.kt
@@ -1,8 +1,10 @@
 package com.tubes.gapedulidilindungi.retrofit
 
+import com.tubes.gapedulidilindungi.NewsModel
+import retrofit2.Call
 import retrofit2.http.GET
 
 interface ApiEndpoint {
     @GET("get-news")
-    fun getNews()
+    fun getNews():Call<List<NewsModel>>
 }
