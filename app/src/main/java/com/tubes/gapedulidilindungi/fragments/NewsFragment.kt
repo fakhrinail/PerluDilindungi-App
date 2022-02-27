32940c7312f24a7c3f6cd8fd591c627c82e1b884
haikallf <haikallzrd@gmail.com>
2022-02-28 00:19:12 +0700

    add recyclerview in a fragment (list of news)

---
+++ app/src/main/java/com/tubes/gapedulidilindungi/fragments/NewsFragment.kt
@@ -0,0 +1,96 @@
+package com.tubes.gapedulidilindungi.fragments
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.FragmentTransaction
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.tubes.gapedulidilindungi.*
+import com.tubes.gapedulidilindungi.retrofit.ApiService
+import kotlinx.android.synthetic.main.fragment_news.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class NewsFragment : Fragment() {
+    private val TAG: String = "NewsActivity"
+
+    lateinit var newsAdapter: NewsAdapter
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_news, container, false)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        setupRecyclerView()
+        getNewsDataFromApi()
+    }
+
+    private fun setupRecyclerView() {
+        newsAdapter = NewsAdapter(arrayListOf(), object : NewsAdapter.OnAdapterListener {
+            override fun onClick(result: NewsModel.Results) {
+                val newsDetailsFragment = NewsDetailsFragment()
+                val bundle = Bundle()
+                bundle.putString("news_url", result.link[0])
+                newsDetailsFragment.arguments = bundle
+
+                if (newsDetailsFragment != null) {
+                    val transaction: FragmentTransaction = fragmentManager!!.beginTransaction()
+                    transaction.replace(R.id.fragment_container, newsDetailsFragment)
+                    transaction.commit()
+                }
+            }
+
+        })
+        recyclerViewNews__newsList.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = newsAdapter
+        }
+    }
+
+    private fun getNewsDataFromApi() {
+        progressBarNews__newsLoading.visibility = View.VISIBLE
+        with(ApiService) {
+            endpoint.getNews()
+                .enqueue(object : Callback<NewsModel> {
+                    override fun onFailure(call: Call<NewsModel>, t: Throwable) {
+                        progressBarNews__newsLoading.visibility = View.GONE
+                        printLog( ">>> onFailure <<< : $t")
+                    }
+                    override fun onResponse(
+                        call: Call<NewsModel>,
+                        response: Response<NewsModel>
+                    ) {
+                        progressBarNews__newsLoading.visibility = View.GONE
+                        if (response.isSuccessful) {
+                            showData( response.body()!! )
+                        }
+                    }
+                })
+        }
+    }
+
+    private fun printLog(message: String) {
+        Log.d(TAG, message)
+    }
+
+    private fun showData (data: NewsModel) {
+        val results = data.results
+        newsAdapter.setData(results)
+//        for (result in results) {
+//            printLog("title: ${result.title}")
+//        }
+    }
+
+}
