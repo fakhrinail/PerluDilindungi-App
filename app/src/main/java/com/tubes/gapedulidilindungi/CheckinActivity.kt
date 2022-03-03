492dc37beea2920920f667812d186afb2a10cc1c
fakhrinail <fakhrinailwbw@gmail.com>
2022-03-03 13:17:29 +0700

    add access location feature

--- app/src/main/java/com/tubes/gapedulidilindungi/CheckinActivity.kt
+++ app/src/main/java/com/tubes/gapedulidilindungi/CheckinActivity.kt
@@ -1,5 +1,7 @@
 package com.tubes.gapedulidilindungi
 
+import android.Manifest
+import android.content.Context
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -14,9 +16,12 @@ import com.budiyev.android.codescanner.CodeScanner
 import com.budiyev.android.codescanner.DecodeCallback
 import com.budiyev.android.codescanner.ErrorCallback
 import com.budiyev.android.codescanner.ScanMode
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.tubes.gapedulidilindungi.models.ProvinceCityModel
 import com.tubes.gapedulidilindungi.retrofit.ApiService
 import kotlinx.android.synthetic.main.activity_checkin.*
+import kotlinx.android.synthetic.main.fragment_location.*
 import okhttp3.RequestBody
 import okhttp3.ResponseBody
 import retrofit2.Call
@@ -25,13 +30,17 @@ import retrofit2.Response
 
 class CheckinActivity : AppCompatActivity() {
     private lateinit var codeScanner: CodeScanner
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_checkin)
 
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this@CheckinActivity)
+
         setupPermissions()
         codeScanner()
+        getLastKnownLocation()
     }
 
     private fun codeScanner() {
@@ -54,7 +63,11 @@ class CheckinActivity : AppCompatActivity() {
 
             errorCallback = ErrorCallback {
                 runOnUiThread {
-                    Toast.makeText(this@CheckinActivity, "Unable to post data to server", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(
+                        this@CheckinActivity,
+                        "Unable to post data to server",
+                        Toast.LENGTH_SHORT
+                    ).show()
                     Log.e("Main", "codeScanner: ${it.message}")
                 }
             }
@@ -74,6 +87,39 @@ class CheckinActivity : AppCompatActivity() {
     override fun onPause() {
         codeScanner.releaseResources()
         super.onPause()
+    }
+
+    private fun getLastKnownLocation() {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+                Log.d("Location", "No permission granted")
+            return
+        } else {
+            Log.d("Location", "Permission granted")
+            fusedLocationClient.lastLocation
+                .addOnSuccessListener { location ->
+                    if (location != null) {
+                        Log.d("CheckinActivity", location.toString())
+                        Log.d("Location", location.latitude.toString())
+                        Log.d("Location", location.longitude.toString())
+                        // use your location object
+                        // get latitude , longitude and other info from this
+                    }
+                }
+        }
     }
 
     private fun postCheckin(requestBody: RequestBody) {
@@ -97,7 +143,11 @@ class CheckinActivity : AppCompatActivity() {
                         } else {
                             Log.d("CheckinActivity", response.body().toString())
                             Log.d("CheckinActivity", response.message())
-                            Toast.makeText(this@CheckinActivity, response.message(), Toast.LENGTH_SHORT)
+                            Toast.makeText(
+                                this@CheckinActivity,
+                                response.message(),
+                                Toast.LENGTH_SHORT
+                            )
                                 .show()
                         }
                     }
@@ -106,18 +156,37 @@ class CheckinActivity : AppCompatActivity() {
     }
 
     private fun setupPermissions() {
-        val permission = ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA)
+        val cameraPermission =
+            ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA)
+        val locationPermission = ContextCompat.checkSelfPermission(
+            this,
+            android.Manifest.permission.ACCESS_COARSE_LOCATION
+        )
 
-        if (permission != PackageManager.PERMISSION_GRANTED) {
-            makeRequest()
+        if (cameraPermission != PackageManager.PERMISSION_GRANTED) {
+            makeRequest(CAMERA_REQ)
+        }
+
+        if (locationPermission != PackageManager.PERMISSION_GRANTED) {
+            makeRequest(LOCATION_REQ)
         }
     }
 
-    private fun makeRequest() {
-        ActivityCompat.requestPermissions(
-            this, arrayOf(android.Manifest.permission.CAMERA),
-            CAMERA_REQ
-        )
+    private fun makeRequest(requestCode: Int) {
+        when (requestCode) {
+            CAMERA_REQ -> {
+                ActivityCompat.requestPermissions(
+                    this, arrayOf(android.Manifest.permission.CAMERA),
+                    CAMERA_REQ
+                )
+            }
+            LOCATION_REQ -> {
+                ActivityCompat.requestPermissions(
+                    this, arrayOf(android.Manifest.permission.ACCESS_COARSE_LOCATION),
+                    LOCATION_REQ
+                )
+            }
+        }
     }
 
     override fun onRequestPermissionsResult(
@@ -131,7 +200,16 @@ class CheckinActivity : AppCompatActivity() {
                 if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
                     Toast.makeText(
                         this,
-                        "You need the camera permission to use this app",
+                        "You need the camera permission to use this feature",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+            LOCATION_REQ -> {
+                if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(
+                        this,
+                        "You need the location permission to use this feature",
                         Toast.LENGTH_SHORT
                     ).show()
                 }
@@ -141,5 +219,6 @@ class CheckinActivity : AppCompatActivity() {
 
     companion object {
         private const val CAMERA_REQ = 101
+        private const val LOCATION_REQ = 99
     }
 }
